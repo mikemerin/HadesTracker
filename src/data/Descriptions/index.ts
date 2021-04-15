8a48752ea4e72afac936d0db854ce80b5ccc026b
Mike Merin <mike.d.merin@gmail.com>
2021-04-14 20:25:43 -0400

    Connected upgrades to display

--- src/data/Descriptions/index.ts
+++ src/data/Descriptions/index.ts
@@ -1,12 +1,13 @@
 import { Mapping } from 'redux/domain';
 import { chaosBoonDescriptions } from './chaosBoons';
 import { boonDescriptions } from './boons';
-import { aspectDescriptions } from './weapons';
+import { aspectDescriptions, upgradeDescriptions } from './weapons';
 
 const descriptions: Mapping = {
   ...aspectDescriptions,
   ...boonDescriptions,
   ...chaosBoonDescriptions,
+  ...upgradeDescriptions,
 };
 
 export {
